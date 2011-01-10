013f372fec1bae616109544fa7e84fb713bcca46
zulusnet <zulus@zas.pl>
2011-01-11 00:19:06 +0100

    Michal Rogulski, mrogulsk, dodanie modelu friend

---
+++ cakephp/app/models/friend.php
@@ -0,0 +1,12 @@
+<?php
+	class Friend extends AppModel {
+		var $name = 'Friend';
+		/* var $belongsTo = array(
+			'User' => array(
+				'className'  => 'User',
+				'foreignKey' => 'user_id'
+			)
+		); 
+		*/
+	}
+?>
