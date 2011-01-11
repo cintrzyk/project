5fa3a40f56c34d052e7df700e0a92e02b12cdcd4
zulusnet <zulus@zas.pl>
2011-01-11 23:37:07 +0100

    Michal Rogulski, mrogulsk, poprawki w profiles_controller

--- cakephp/app/controllers/profiles_controller.php
+++ cakephp/app/controllers/profiles_controller.php
@@ -1,16 +1,15 @@
 <?php
 class ProfilesController extends AppController {
 
-	var $components = array('Session');
+	var $components = array('Session', 'Attachment' => array('default_col' => 'profile'));
 	var $helpers = array('Html', 'Form');
 	var $name = 'Profiles';
 	function index() {
 		$this->set('profiles', $this->Profile->find('all'));
 	}
-	
-	function profile() {
-		//print_r($this->Session->read());
-		$this->Profile->id = $this->Session->read('zalogowany.id');
+
+	function show($id = null) {
+		$this->Profile->id = $id;
 		$this->set('profile', $this->Profile->read());
 	}
 	
@@ -18,10 +17,35 @@ class ProfilesController extends AppController {
 		if (!empty($dane)) {
 			$this->Attachment->upload($dane['Profile']);
 			$this->Profile->save($dane);
-			$this->Session->setFlash('Profil został pomyślnie dodany');
-			// $this->redirect(array('action' => 'index'));
+				$this->Session->setFlash('Profil został pomyślnie dodany');
+				//$this->redirect(array('action' => 'index'));
 		}
 	}
+	
+	function deleteFriend($id = null) {
+		$dousuniecia = $this->Profile->query("SELECT * FROM friends WHERE (friends.profile_id=".$this->Session->read('zalogowany.id')." AND friends.friend_id=".$id.") OR (friends.profile_id=".$id." AND friends.friend_id=".$this->Session->read('zalogowany.id').")");
+		//$dousuniecia = $this->Profile->Friend->findByProfileId($id);
+		//$this->Session->write('test',$dousuniecia);
+		$this->Session->setFlash('Znajomy został pomyślnie usunięty.');
+		$this->Profile->Friend->delete($dousuniecia[0]['friends']['id']);
+		
+		$this->redirect(array('action' => 'profile'));
+	}
+	
+	function edit($id = null) {
+		if ($id == null) {
+			$this->Profile->id = $this->Session->read('zalogowany.id');
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Profile->read();
+		} else {
+			$this->Attachment->upload($this->data['Profile']);
+			$this->Profile->save($this->data);
+			$this->Session->setFlash('Profil został zaktualizowany.');
+			$this->redirect(array('action' => 'profile'));
+		}
+	}	
+	
 	
 }
 ?>
