589cedd9b51eac22bfb1c182afabf4caf97f2e94
cintrzyk <cin3k@wp.pl>
2011-01-12 00:50:30 +0100

    Marcin Brzezinski, mbrzezin, aktualizacja metody show()

--- cakephp/app/controllers/profiles_controller.php
+++ cakephp/app/controllers/profiles_controller.php
@@ -9,70 +9,24 @@ class ProfilesController extends AppController {
 	}
 	
 	function profile() {
-		//print_r($this->Session->read());
-		$this->Profile->id = $this->Session->read('zalogowany.id');
+		$this->Profile->id = $this->Session->read('zalogowany.User.id');
 		$this->set('profile', $this->Profile->read());
-		
-		$znajomi = $this->Profile->Friend->findAllByProfileId($this->Session->read('zalogowany.id'));
-		$znajomi = $this->Profile->query("SELECT * FROM profiles, friends WHERE (friends.profile_id=".$this->Session->read('zalogowany.id')." AND friends.friend_id=profiles.id) OR (friends.profile_id=profiles.id AND friends.friend_id=".$this->Session->read('zalogowany.id').")");
-		if(!empty($znajomi)) {
-			$this->set('friends', $znajomi); // $this->Profile->findAllById($znajomi['Friend']['friend_id'])
-		}
 	}
 	
-	function search() {
-		$znalezione = $this->Profile->findAllByImie($this->data['Profile']['zapytanie']);
-		$this->Session->write('zapytanie', $this->data['Profile']['zapytanie']);
-		if (!empty($znalezione)) {
-			$this->set('profiles', $znalezione);
-		}
-	}
-	
-	function addFriend($id = null) {
-		$this->data['Friend']['friend_id'] = $id;
-		$this->data['Friend']['profile_id'] = $this->Session->read('zalogowany.id');
-		$this->Profile->Friend->save($this->data);
-		$this->redirect(array('action' => 'profile'));
-	}
-
 	function show($id = null) {
 		$this->Profile->id = $id;
+		//$this->Profile->id = $this->Session->read('zalogowany.User.id');
 		$this->set('profile', $this->Profile->read());
 	}
 	
-	function add($dane = null) {
-		if (!empty($dane)) {
-			$this->Attachment->upload($dane['Profile']);
-			$this->Profile->save($dane);
-				$this->Session->setFlash('Profil został pomyślnie dodany');
-				//$this->redirect(array('action' => 'index'));
-		}
-	}
-	
-	function deleteFriend($id = null) {
-		$dousuniecia = $this->Profile->query("SELECT * FROM friends WHERE (friends.profile_id=".$this->Session->read('zalogowany.id')." AND friends.friend_id=".$id.") OR (friends.profile_id=".$id." AND friends.friend_id=".$this->Session->read('zalogowany.id').")");
-		//$dousuniecia = $this->Profile->Friend->findByProfileId($id);
-		//$this->Session->write('test',$dousuniecia);
-		$this->Session->setFlash('Znajomy został pomyślnie usunięty.');
-		$this->Profile->Friend->delete($dousuniecia[0]['friends']['id']);
-		
-		$this->redirect(array('action' => 'profile'));
-	}
-	
-	function edit($id = null) {
-		if ($id == null) {
-			$this->Profile->id = $this->Session->read('zalogowany.id');
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Profile->read();
-		} else {
+	function add() {
+		if (!empty($this->data)) {
 			$this->Attachment->upload($this->data['Profile']);
 			$this->Profile->save($this->data);
-			$this->Session->setFlash('Profil został zaktualizowany.');
-			$this->redirect(array('action' => 'profile'));
+				$this->Session->setFlash('Profil został pomyślnie dodany');
+				$this->redirect(array('action' => 'index'));
 		}
-	}	
-	
+	}
 	
 }
 ?>
